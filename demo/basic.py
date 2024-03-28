3bad52f273ade82da3e796933cf24a37a6b5faa1
Graeme Winter <graeme.winter@gmail.com>
2024-03-28 08:25:57 +0000

    Less trivial example

--- demo/basic.py
+++ demo/basic.py
@@ -1,7 +1,9 @@
 import h5py
 import numpy
 
-secret = numpy.array((1, 2, 3, 4, 5), dtype=numpy.uint16)
+secret = numpy.array(numpy.zeros((1024, 1024)), dtype=numpy.uint8)
+
+secret[511][:5] = (1, 2, 3, 4, 5)
 
 with h5py.File("secret.h5", "w") as f:
-    p = f.create_dataset("password", data=secret, chunks=(5,))
+    p = f.create_dataset("password", data=secret, chunks=(1, 1024), compression="gzip")
