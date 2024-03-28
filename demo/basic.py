57a4887a21164d4e7606f0c4e7228ab62f60a79a
Graeme Winter <graeme.winter@gmail.com>
2024-03-28 10:15:22 +0000

    Better basic demo

--- demo/basic.py
+++ demo/basic.py
@@ -1,9 +1,10 @@
 import h5py
 import numpy
 
-secret = numpy.array(numpy.zeros((1024, 1024)), dtype=numpy.uint8)
+data = numpy.zeros((512, 512, 512), dtype=numpy.uint16)
 
-secret[511][:5] = (1, 2, 3, 4, 5)
+for j in range(512):
+    data[j, :, :] = j
 
-with h5py.File("secret.h5", "w") as f:
-    p = f.create_dataset("password", data=secret, chunks=(1, 1024), compression="gzip")
+with h5py.File("data.h5", "w") as f:
+    p = f.create_dataset("data", data=data, chunks=(1, 512, 512), compression="gzip")
